7ea013b38f624fc534f218e8544051bfda961fbb
sebastian correa <sebastian@sawyereffect.com>
2020-01-06 20:51:29 -0500

    working on the server.js

--- fractal.js
+++ fractal.js
@@ -47,8 +47,30 @@ fractal.web.set('static.path', path.join(__dirname, 'public'));
 
 fractal.web.set('builder.dest', __dirname + '/build');
 
-    // const server = fractal.web.server({});
 
+
+    const server = fractal.web.server({});
+
+    // server.use('/',function (err, req, res, next) {
+    //     console.error(err.stack)
+    //   })
+    // server.use('/',function (req, res, next) {
+    //     console.log('middleware')
+    //     next()
+    //   })
+    
+      var auth = require('http-auth');
+      var basic = auth.basic({
+      },(username, password, callback) => { 
+          console.log(username, password, callback)
+        // Custom authentication
+        // Use callback(error) if you want to throw async error.
+        callback(username === "Tina" && password === "Bullock");
+    });
+
+    var midd = auth.connect(basic)
+    // server._server.use.apply(server._server,midd);
+    server._server.use(midd);
     // // server.use(auth.connect(basic))
 
     // const basicAuth = require('express-basic-auth')
@@ -58,43 +80,39 @@ fractal.web.set('builder.dest', __dirname + '/build');
 
 
 
-    // server.on('ready', () => {
-    //     // const header = 'Fractal web UI server is running!';
-    //     // // const footer = this.fractal.cli.isInteractive() ? 'Use the \'stop\' command to stop the server.' : 'Use ^C to stop the server.';
-    //     // const serverUrl = server.urls.server;
-    //     // const format = str => this.console.theme.format(str, 'success', true);
-    //     // let body = '';
+    server.on('ready', () => {
+        // const header = 'Fractal web UI server is running!';
+        // // const footer = this.fractal.cli.isInteractive() ? 'Use the \'stop\' command to stop the server.' : 'Use ^C to stop the server.';
+        // const serverUrl = server.urls.server;
+        // const format = str => this.console.theme.format(str, 'success', true);
+        // let body = '';
 
-    //     // if (!server.isSynced) {
-    //     //     body += `Local URL: ${format(serverUrl)}`;
-    //     // } else {
-    //     //     const syncUrls = server.urls.sync;
-    //     //     body += `Local URL:      ${format(syncUrls.local)}`;
-    //     //     body += `\nNetwork URL:    ${format(syncUrls.external)}`;
-    //     //     body += `\nBrowserSync UI: ${format(syncUrls.ui)}`;
-    //     // }
+        // if (!server.isSynced) {
+        //     body += `Local URL: ${format(serverUrl)}`;
+        // } else {
+        //     const syncUrls = server.urls.sync;
+        //     body += `Local URL:      ${format(syncUrls.local)}`;
+        //     body += `\nNetwork URL:    ${format(syncUrls.external)}`;
+        //     body += `\nBrowserSync UI: ${format(syncUrls.ui)}`;
+        // }
 
-    //     // return this.console.box(header, body, '').persist();
-    //     console.log('runnning')
-    // });
+        // return this.console.box(header, body, '').persist();
+        console.log('runnning')
+    });
 
-    // server.on('error', (err, req) => {
-    //     if (err.status === '404') {
-    //         this.console.warn(`404: ${err.message}`);
-    //     } else {
-    //         this.console.error(err.message, err);
-    //     }
-    //     done();
-    // });
+    server.on('error', (err, req) => {
+        if (err.status === '404') {
+            this.console.warn(`404: ${err.message}`);
+        } else {
+            this.console.error(err.message, err);
+        }
+        done();
+    });
 
-    // server.on('destroy', () => done());
-    // server.on('stopped', () => done());
+    server.on('destroy', () => done());
+    server.on('stopped', () => done());
 
-    // // var basic = auth.basic({
-    // //     file: path.join(__dirname, 'user.htpasswd')
-    // // });
-
-    // server.start(false).catch(e => {
-    //     this.console.error(e);
-    //     done();
-    // });
+    server.start(false).catch(e => {
+        this.console.error(e);
+        done();
+    });
