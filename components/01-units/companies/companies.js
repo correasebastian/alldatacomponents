f6ddcbf493350c313e1d7d23bee2518741d186b0
sebastian correa <sebastian.c@borngroup.com>
2020-01-14 15:00:45 -0700

    companies-slider

--- components/01-units/companies/companies.js
+++ components/01-units/companies/companies.js
@@ -1,5 +1,6 @@
 "use strict";
-
+import { tns } from "tiny-slider/src/tiny-slider";
+import $ from "jquery";
 /**
  *  This is the main file for Companies
  */
@@ -14,3 +15,35 @@ class Companies {
 
 */
 
+$(document).ready(function() {
+  var sliderLg = tns({
+    container: ".companies-slider-lg",
+    nav: false,
+    loop: false,
+    rewind: true,
+    mouseDrag: true,
+    prevButton: "#prev-lg",
+    nextButton: "#next-lg",
+    items: 5,
+    slideBy: "page",
+    swipeAngle: false,
+    speed: 400
+  });
+
+  var sliderMob = tns({
+    container: ".companies-slider-mob",
+    controlsPosition: "bottom",
+    nav: true,
+    navContainer: "#mov-nav",
+    navPosition: "bottom",
+    loop: false,
+    rewind: true,
+    mouseDrag: true,
+    prevButton: "#prev-mob",
+    nextButton: "#next-mob",
+    items: 1,
+    slideBy: "page",
+    swipeAngle: false,
+    speed: 400
+  });
+});
