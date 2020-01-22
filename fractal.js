91e371617dc83cb089da313ca52b327f88706893
sebastian correa <sebastian@sawyereffect.com>
2020-01-22 10:03:07 -0500

    usingprefix

--- fractal.js
+++ fractal.js
@@ -1,53 +1,57 @@
-'use strict';
+"use strict";
 
 /*
-* Require the path module
-*/
-const path = require('path');
+ * Require the path module
+ */
+const path = require("path");
 
 /*
  * Require the Fractal module
  */
-const fractal = module.exports = require('@frctl/fractal').create();
-const twigAdapter = require('@frctl/twig')();
+const fractal = (module.exports = require("@frctl/fractal").create());
+const twigAdapter = require("@frctl/twig")({
+  handlePrefix: "@components/"
+  // namespaces: {
+  //     '@components': 'components/01-units/threelines'
+  // },
+});
 
-var isProd = process.env.NODE_ENV === 'production'
+var isProd = process.env.NODE_ENV === "production";
 
-var auth = require('http-auth');
-
-
+var auth = require("http-auth");
 
 fractal.components.engine(twigAdapter);
-fractal.components.set('ext', '.twig');
-fractal.components.set('default.preview','@preview' /* isProd ? '@previewprod' :'@preview' */);
+fractal.components.set("ext", ".twig");
+fractal.components.set(
+  "default.preview",
+  "@preview" /* isProd ? '@previewprod' :'@preview' */
+);
 
 /*
  * Give your project a title.
  */
-fractal.set('project.title', 'alldata components');
+fractal.set("project.title", "alldata components");
 
 /*
  * Tell Fractal where to look for components.
  */
-fractal.components.set('path', path.join(__dirname, 'components'));
+fractal.components.set("path", path.join(__dirname, "components"));
 
 /*
  * Tell Fractal where to look for documentation pages.
  */
-fractal.docs.set('path', path.join(__dirname, 'docs'));
+fractal.docs.set("path", path.join(__dirname, "docs"));
 
 /*
  * Tell the Fractal web preview plugin where to look for static assets.
  */
-fractal.web.set('static.path', path.join(__dirname, 'public'));
+fractal.web.set("static.path", path.join(__dirname, "public"));
 
+fractal.web.set("builder.dest", __dirname + "/build");
 
-fractal.web.set('builder.dest', __dirname + '/build');
-
-if(isProd ){
-    fractal.web.set('auth', {
-        user:'born',
-        pass: 'alldata'
-    }); 
-
+if (isProd) {
+  fractal.web.set("auth", {
+    user: "born",
+    pass: "alldata"
+  });
 }
